bea795285e88af1aadc5541db1ada7f7c123f8fa
bentoco <bentoco@outlook.com.br>
2024-07-16 07:14:02 -0300

    refactor: change app name

---
+++ src/main/java/blog/highcoders/spring_java_exception_handling/Application.java
@@ -0,0 +1,13 @@
+package blog.highcoders.spring_java_exception_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/blog/highcoders/spring_java_exception_handling/ApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.highcoders.spring_java_exception_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
